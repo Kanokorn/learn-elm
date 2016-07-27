67488a83297ea57cbb010cb1c3cfa69319cd6e79
Kanokon Chongnguluam <kanokorn.may@gmail.com>
2016-07-27 14:35:46 +0700

    Check that the password is longer than 8 characters.

---
+++ forms/Forms.elm
@@ -0,0 +1,78 @@
+module Forms exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import String exposing (..)
+
+
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , password : String
+    , passwordAgain : String
+    }
+
+
+model : Model
+model =
+    Model "" "" ""
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Name name ->
+            { model | name = name }
+
+        Password password ->
+            { model | password = password }
+
+        PasswordAgain password ->
+            { model | passwordAgain = password }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ type' "text", placeholder "Name", onInput Name ] []
+        , input [ type' "password", placeholder "Password", onInput Password ] []
+        , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        , viewValidation model
+        ]
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+    let
+        ( color, message ) =
+            if String.length model.password > 8 || String.length model.passwordAgain > 8 then
+                ( "red", "Passwords is longer than 8 characters" )
+            else if model.password == model.passwordAgain then
+                ( "green", "OK" )
+            else
+                ( "red", "Passwords do not match!" )
+    in
+        div [ style [ ( "color", color ) ] ] [ text message ]
